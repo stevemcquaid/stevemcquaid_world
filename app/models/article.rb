fdc01b1d284ab7848d3ceb27074f93d7b2ba9fb3
Rintaro Sato <rintaros@andrew.cmu.edu>
2012-04-26 20:41:51 -0400

    updated models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ActiveRecord::Base
+  belongs_to :categories
+  validates_presence_of :title, :content
+  
+  scope :alphabetical, order('title')
+  scope :active, where('active = ?', true)
 end
